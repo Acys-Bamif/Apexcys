76473fe99c7f3e027959a20f0b46d055593d5bee
Acys Bamif <104174570+Acys-Bamif@users.noreply.github.com>
2024-01-14 20:22:14 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,99 @@
+/*Acys bamif script.js 14.01.2023*/
+
+var slideIndex = -1,
+	curSlide = document.getElementsByClassName('services'),
+	start = '';
+
+// show form modal
+function openModal() {
+	modal.style.display = "block";
+}
+
+//hide form modal
+function hideModal() {
+	modal.style.display = "none"
+}
+
+(function () {
+	"use strict";
+	
+// define variables
+	var h2s = document.querySelectorAll(".sub-head");
+	
+// check if an element is in viewport
+// http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+	function isElementInViewport(el) {
+		var rect = el.getBoundingClientRect();
+		return (
+			rect.top >= 0 &&
+			rect.left >= 0 &&
+			rect.bottom <=
+			(window.innerHeight || document.documentElement.clientHeight) &&
+			rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+		);
+	}
+	
+//initialize the default look with js so that it does not affect the look when script is disabled
+	for (var i = 0; i < h2s.length; i++) {
+		h2s[i].classList.add("animate-h2");
+	}
+		
+//call back function codes
+	function callbackFunc() {
+		for (var i = 0; i < h2s.length; i++) {
+			if (isElementInViewport(h2s[i])) {
+				h2s[i].classList.add("h2-inview");
+			}
+			else {
+				h2s[i].classList.remove("h2-inview");
+			}
+		}
+	}
+	
+	// listen for events
+	window.addEventListener("load", callbackFunc);
+	window.addEventListener("resize", callbackFunc);
+	window.addEventListener("scroll", callbackFunc);
+})();
+
+(function () {
+	"use strict";
+	
+// define variables
+	var h2s = document.querySelectorAll(".qualities-div div h3");
+	
+// check if an element is in viewport
+// http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+	function isElementInViewport(el) {
+		var rect = el.getBoundingClientRect();
+		return (
+			rect.top >= 0 &&
+			rect.left >= 0 &&
+			rect.bottom <=
+			(window.innerHeight || document.documentElement.clientHeight) &&
+			rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+		);
+	}
+	
+//initialize the default look with js so that it does not affect the look when script is disabled
+	for (var i = 0; i < h2s.length; i++) {
+		h2s[i].parentElement.parentElement.classList.add("animate-s-card");
+	}
+		
+//call back function codes
+	function callbackFunc() {
+		for (var i = 0; i < h2s.length; i++) {
+			if (isElementInViewport(h2s[i])) {
+				h2s[i].parentElement.parentElement.classList.add("s-card-inview");
+			}
+			else {
+				h2s[i].parentElement.parentElement.classList.remove("s-card-inview");
+			}
+		}
+	}
+	
+	// listen for events
+	window.addEventListener("load", callbackFunc);
+	window.addEventListener("resize", callbackFunc);
+	window.addEventListener("scroll", callbackFunc);
+})();
