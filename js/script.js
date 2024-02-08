98c3a3df8376668b0ca6009faa9e14544bd89d23
Acys Bamif <104174570+Acys-Bamif@users.noreply.github.com>
2024-02-08 18:28:29 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,9 @@
-/*Acys bamif script.js 14.01.2023*/
+/*Acys bamif script.js 08.02.2023*/
 
-var slideIndex = -1,
-	curSlide = document.getElementsByClassName('services'),
-	start = '';
-
-// show form modal
-function openModal() {
-	modal.style.display = "block";
-}
-
-//hide form modal
-function hideModal() {
-	modal.style.display = "none"
-}
-
-(function () {
+function jsAnimate(h2s, inview, animate) {
 	"use strict";
 	
 // define variables
-	var h2s = document.querySelectorAll(".sub-head");
 	
 // check if an element is in viewport
 // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
@@ -35,17 +20,14 @@ function hideModal() {
 	
 //initialize the default look with js so that it does not affect the look when script is disabled
 	for (var i = 0; i < h2s.length; i++) {
-		h2s[i].classList.add("animate-h2");
+		h2s[i].classList.add(animate);
 	}
 		
 //call back function codes
 	function callbackFunc() {
 		for (var i = 0; i < h2s.length; i++) {
 			if (isElementInViewport(h2s[i])) {
-				h2s[i].classList.add("h2-inview");
-			}
-			else {
-				h2s[i].classList.remove("h2-inview");
+				h2s[i].classList.add(inview);
 			}
 		}
 	}
@@ -54,46 +36,12 @@ function hideModal() {
 	window.addEventListener("load", callbackFunc);
 	window.addEventListener("resize", callbackFunc);
 	window.addEventListener("scroll", callbackFunc);
-})();
+};
 
-(function () {
-	"use strict";
-	
-// define variables
-	var h2s = document.querySelectorAll(".qualities-div div h3");
-	
-// check if an element is in viewport
-// http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
-	function isElementInViewport(el) {
-		var rect = el.getBoundingClientRect();
-		return (
-			rect.top >= 0 &&
-			rect.left >= 0 &&
-			rect.bottom <=
-			(window.innerHeight || document.documentElement.clientHeight) &&
-			rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-		);
-	}
-	
-//initialize the default look with js so that it does not affect the look when script is disabled
-	for (var i = 0; i < h2s.length; i++) {
-		h2s[i].parentElement.parentElement.classList.add("animate-s-card");
-	}
-		
-//call back function codes
-	function callbackFunc() {
-		for (var i = 0; i < h2s.length; i++) {
-			if (isElementInViewport(h2s[i])) {
-				h2s[i].parentElement.parentElement.classList.add("s-card-inview");
-			}
-			else {
-				h2s[i].parentElement.parentElement.classList.remove("s-card-inview");
-			}
-		}
-	}
-	
-	// listen for events
-	window.addEventListener("load", callbackFunc);
-	window.addEventListener("resize", callbackFunc);
-	window.addEventListener("scroll", callbackFunc);
-})();
+jsAnimate(document.querySelectorAll(".header-inner>p"), 'h2-inview', 'animate-h2');
+jsAnimate(document.querySelectorAll(".sub-head"), 'h2-inview', 'animate-h2');
+jsAnimate(document.querySelectorAll("#portfolio img"), 'p-card-inview', 'animate-p-card');
+jsAnimate(document.querySelectorAll("#testimonial>div>div"), 't-card-inview', 'animate-t-card');
+jsAnimate(document.querySelectorAll("#service>div>div"), 't-card-inview', 'animate-t-card');
+jsAnimate(document.querySelectorAll("#process>div>div"), 't-card-inview', 'animate-t-card');
+jsAnimate(document.querySelectorAll("#cta>div"), 't-card-inview', 'animate-t-card');
